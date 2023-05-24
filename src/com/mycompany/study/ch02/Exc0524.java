08d70f3504db6b3f95c57f9a6495cfde5f62f151
MJ0629 <ymj910629@naver.com>
2023-05-24 17:40:28 +0900

    23.05.24

---
+++ src/com/mycompany/study/ch02/Exc0524.java
@@ -0,0 +1,14 @@
+package com.mycompany.study.ch02;
+
+public class Exc0524 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		boolean a = true;
+		boolean b = false;
+		
+		System.out.println(b+"\n"+a);
+	}
+
+}
--- src/com/mycompany/study/ch02/Helloworld.java
+++ src/com/mycompany/study/ch02/Helloworld.java
@@ -4,8 +4,23 @@ public class Helloworld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		float myFloatNum = 5.99f; 
+		System.out.println(myFloatNum);
 		
-		System.out.println("이름 : 윤미진 , 나이 : 33 , 주소 : 김포시 북변동");
+		boolean a = true;
+		// 원시형 타입 등호 앞은 변수 선언 등호 뒤는 변수 초기화
+		// 변수 선언 : 데이터타입 변수명;
+		// 변수 초기화 : 데이터타입 변수명 = 리터럴;
+		boolean b = false;
+		
+		int c = 1;
+		
+		int e,
+			r,
+			g
+			;
+		g= 1;
+		System.out.println("e:" + g);
 		
 	}
 
