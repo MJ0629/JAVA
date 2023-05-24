81cebf2b9ed1dbd8103ce12cd4b0076b3480b690
MJ0629 <ymj910629@naver.com>
2023-05-24 15:37:27 +0900

    23.05.24

---
+++ src/com/mycompany/study/ch02/Helloworld.java
@@ -0,0 +1,12 @@
+package com.mycompany.study.ch02;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("이름 : 윤미진 , 나이 : 33 , 주소 : 김포시 북변동");
+		
+	}
+
+}
