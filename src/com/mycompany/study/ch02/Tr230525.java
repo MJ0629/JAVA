b410266171f8b40c67cb435cb17e1672c2a9859a
MJ0629 <ymj910629@naver.com>
2023-05-25 17:32:05 +0900

    23.05.25

---
+++ src/com/mycompany/study/ch02/Tr230525.java
@@ -0,0 +1,29 @@
+package com.mycompany.study.ch02;
+
+public class Tr230525 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		boolean defaultNy = true;
+		System.out.println(defaultNy);
+		
+		byte byteVar = 10;
+		System.out.println(byteVar);
+		
+		char c1 = 'A';
+		System.out.println(c1);
+		
+		float floatVar = 10F;
+		double doubleVar = 123456789;
+		System.out.println(floatVar);
+		System.out.println(doubleVar);
+		
+		int intVar = byteVar;
+		System.out.println(intVar);
+		
+		long longVar = 1010101010L;
+		System.out.println(longVar);
+	}
+
+}
